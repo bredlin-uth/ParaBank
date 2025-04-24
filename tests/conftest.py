f8488fd9aa4fc01c8d41bff188cae96d2b9f4de3
harshitha <126103647+uth-testautomation@users.noreply.github.com>
2025-04-24 17:41:38 +0530

    updated conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,9 @@ from selenium.common import InvalidArgumentException
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
-
-
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
 # Custom pytest command-line option to specify the browser
 # Usage: pytest --browser chrome/firefox/edge
@@ -32,13 +33,16 @@ def setup_and_teardown(request):
     if browser == "chrome":
         options = webdriver.ChromeOptions()
         options.add_experimental_option('detach', True)
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
     elif browser == "edge":
         options = webdriver.EdgeOptions()
-        driver = webdriver.Edge(EdgeChromiumDriverManager().install(), options=options)
+        service = EdgeService(EdgeChromiumDriverManager().install())
+        driver = webdriver.Edge(service=service, options=options)
     elif browser == "firefox":
         options = webdriver.FirefoxOptions()
-        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
+        service = FirefoxService(GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=service, options=options)
     else:
         raise InvalidArgumentException(f"Invalid browser selection: {browser}")
 
